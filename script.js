441b430e1c671d9af8b78327d6258fab34110e29
Echa Apriliyanto <apriliyantoecha1@gmail.com>
2024-09-16 14:44:17 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,391 @@
+const streamingPlatforms = [
+  {
+    platform: 'Spotify',
+    logoPath: './assets/image/logos/spotify.png',
+    href: 'https://open.spotify.com/intl-id/artist/4UK2Lzi6fBfUi9rpDt6cik',
+  },
+  {
+    platform: 'YouTube Music',
+    logoPath: './assets/image/logos/yt-music.png',
+    href: 'https://music.youtube.com/channel/UCabLXblrQG4cO8F9qdd4Xsw',
+  },
+  {
+    platform: 'Apple Music',
+    logoPath: './assets/image/logos/apple-music.png',
+    href: 'https://music.apple.com/us/artist/yorushika/1250709916',
+  },
+  {
+    platform: 'Tidal',
+    logoPath: './assets/image/logos/tidal.png',
+    href: 'https://tidal.com/browse/artist/16041403',
+  },
+  {
+    platform: 'Amazon Music',
+    logoPath: './assets/image/logos/amazon-music.png',
+    href: 'https://music.amazon.com/artists/B07T52LKZ1/yorushika',
+  },
+  {
+    platform: 'AWA',
+    logoPath: './assets/image/logos/awa.png',
+    href: 'https://awa.fm/',
+  },
+  {
+    platform: 'Line Music',
+    logoPath: './assets/image/logos/line-music.png',
+    href: 'https://music.line.me/webapp/artist/mi0000000010dee575',
+  },
+  {
+    platform: 'Qobuz',
+    logoPath: './assets/image/logos/qobuz.png',
+    href: 'https://www.qobuz.com/us-en/interpreter/yorushika/4595601',
+  },
+];
+
+const discography = [
+  {
+    title: '【配信】Digital Single「忘れてください」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/00b49604d8e74a67014951d1de1e7a51.jpeg',
+    date: '2024.07.13',
+  },
+  {
+    title:
+      '【配信】Digital Single「若者のすべて」（フジファブリック原曲カバー）',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/5515b6ef92bc6d5c77ba6b3ab45a7a19.jpeg',
+    date: '2024.06.21',
+  },
+  {
+    title: '【配信】Digital Single「ルバート」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/dfd603e25300e7a2296a40b9a95e9e6f.jpeg',
+    date: '2024.05.29',
+  },
+  {
+    title: '【配信】Digital Single「星めぐる詩」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/c610f33a184e560824fb9b7c94a2f8bf.jpeg',
+    date: '2024.02.23',
+  },
+  {
+    title: '【配信】Digital Single「晴る」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/b3f17612fe0aa314eb1b291cca729865.jpeg',
+    date: '2024.01.05',
+  },
+  {
+    title: '【配信】Digital Single「月光浴」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/3f7f764021ed10dbd76b32fe5d08ce62.jpeg',
+    date: '2023.10.13',
+  },
+  {
+    title: '【配信】Digital Single「斜陽」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/44527d670e625caffda32c4f3343fb62.jpeg',
+    date: '2023.05.08',
+  },
+  {
+    title: '【画集】音楽画集「幻燈」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/000049e271bc349f0a25bd427a43f0c7.jpeg',
+    date: '2023.04.05',
+  },
+  {
+    title: '【配信】Digital Single「Travelers」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/9486c3216351a91f9364d67a5bc97650.jpeg',
+    date: '2023.03.23',
+  },
+  {
+    title: '【配信】Digital Single「451」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/d3fbcf9c93e67df9a6140dd182f967b9.jpeg',
+    date: '2023.03.08',
+  },
+  {
+    title: '【配信】Digital Single「アルジャーノン」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/a6ba3ff42f1a94cfdea66f84c1a7acea.jpeg',
+    date: '2023.02.06',
+  },
+  {
+    title: '【配信】Digital Single「テレパス」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/c9280fd01d555c210f2e309f2810d328.jpeg',
+    date: '2023.01.12',
+  },
+  {
+    title: '【配信】Digital Single「サンサーラ」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/94d8d8f77437a21af42a2240001ca0f5.jpeg',
+    date: '2022.12.23',
+  },
+  {
+    title: '【配信】Digital Single「チノカテ」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/c511e005cea2a91ea4eb512db674a7a7.jpeg',
+    date: '2022.08.29',
+  },
+  {
+    title: '【配信】Digital Single「左右盲」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/c1c98a5d186db54acef3356045d62195.jpeg',
+    date: '2022.07.25',
+  },
+  {
+    title: '【配信】Digital Single「ブレーメン」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/307991377808f74a4094d75d0c26d3e6.jpeg',
+    date: '2022.07.04',
+  },
+  {
+    title: '【Blu-ray & DVD 】Live 「月光」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/a2c91653ae3e7272ee7847002a9fb95f.jpeg',
+    date: '2022.06.29',
+  },
+  {
+    title: '【配信】Digital Single「月に吠える」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/b089a88c1cef0104b52196f2172ecbbf.jpeg',
+    date: '2021.10.06',
+  },
+  {
+    title: '【配信】Digital Single「老人と海」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/5635fc733a83374278e191c30322b124.jpeg',
+    date: '2021.08.18',
+  },
+  {
+    title: '【配信】Digital Single「又三郎」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/f814cf15b0d0fb77a84c0f630616905e.jpeg',
+    date: '2021.06.07',
+  },
+  {
+    title: '【配信】Digital Single「少年時代 (あの夏のルカ ver.)」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/f1f6a3d34e1dae3027238ce1ec2f08c5.jpeg',
+    date: '2021.06.02',
+  },
+  {
+    title: '【DVD&Blu-ray】Live「前世」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/132ae4e9cb6e99b52f9c37a334032edc.jpeg',
+    date: '2021.05.26',
+  },
+  {
+    title: '【EP】「創作」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/1b2e9db3844bbda3f9bb1b38a93ed79a.jpeg',
+    date: '2021.01.27',
+  },
+  {
+    title: '【配信】Digital Single「春泥棒」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/b90538873566813886379469ffb41b6e.jpeg',
+    date: '2021.01.09',
+  },
+  {
+    title: '【配信】Digital Single「風を食む」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/7259eaf9911056366bcc17260e06bc20.png',
+    date: '2020.10.07',
+  },
+  {
+    title: '【ALBUM】3rd Full Album「盗作」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/d46f335b9c9bcc9eadbfd1444e58e113.png',
+    date: '2020.07.29',
+  },
+  {
+    title: '【配信】Digital Single「盗作」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/a812abff3f9a995fd04d96faa248ec01.png',
+    date: '2020.07.22',
+  },
+  {
+    title: '【配信】Digital Single「思想犯」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/238d4d2ef9578018d521e7da266ea617.png',
+    date: '2020.06.24',
+  },
+  {
+    title: '【配信】Digital Single「春ひさぎ」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/c7c674c3c8599d40777e96418ffe81ff.jpeg',
+    date: '2020.06.03',
+  },
+  {
+    title: '【配信】Digital Single「花に亡霊」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/a1333bf75097a387063eece895472664.jpeg',
+    date: '2020.04.22',
+  },
+  {
+    title: '【配信】Digital Single「夜行」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/8e5765cbc347c506f3e44c2ea43a9b2b.jpeg',
+    date: '2020.03.04',
+  },
+  {
+    title: '【ALBUM】2nd Full Album「エルマ」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/e92d3f7feab94300fddae604738c9e02.jpeg',
+    date: '2019.08.28',
+  },
+  {
+    title: '【配信】Digital Single「心に穴が空いた」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/135b70f251bb2c82eaf0d3041669c134.jpeg',
+    date: '2019.06.24',
+  },
+  {
+    title: '【配信】Degital Single「Silence feat. Sarah Furukawa」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/6952a498fc960e2df872f5b40662e85b.jpeg',
+    date: '2019.05.10',
+  },
+  {
+    title: '【ALBUM】1st Full Album「だから僕は音楽を辞めた」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/f75d17e2ec5cebfb81fd579348cc7a11.jpeg',
+    date: '2019.04.10',
+  },
+  {
+    title: '【ALBUM】2nd Mini Album「負け犬にアンコールはいらない」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/9088008c68f0cc13f0024397770a066a.jpeg',
+    date: '2018.05.09',
+  },
+  {
+    title: '【ALBUM】1st Mini Album 「夏草が邪魔をする」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/1f2525fb9bbf8f42904fe4af3a09e87a.jpeg',
+    date: '2017.06.28',
+  },
+  {
+    title: '【ALBUM】2nd Album「月を歩いている」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/117c65b2b4dab0cf36b9024532972819.jpeg',
+    date: '2016.07.06',
+  },
+  {
+    title: '【ALBUM】1st Album「花と水飴、最終電車」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/3bc3f5ba81768e40905e3237f56bf103.jpeg',
+    date: '2015.07.22',
+  },
+  {
+    title: '【ALBUM】同人Album「カーテンコールが止む前に」',
+    imgUrl:
+      'https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/b91080c8ef26ae51385fe1d4b5b1e968.jpeg',
+    date: '2014.04.26',
+  },
+];
+const streamingPlatformsContainer = document.querySelector(
+  '#streaming .streaming-container .platforms'
+);
+let prevScrollpos = window.scrollY;
+const video = document.querySelector('.bg-video');
+const bgImage = document.querySelector('.bg-image');
+
+video.addEventListener('canplaythrough', () => {
+  video.style.display = 'block';
+  bgImage.style.display = 'none';
+});
+
+window.onscroll = function () {
+  const currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById('navbar').style.top = '0';
+  } else {
+    document.getElementById('navbar').style.top = '-300px';
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+for (const stream of streamingPlatforms) {
+  const card = document.createElement('div');
+  const cardImage = document.createElement('img');
+  const cardTitle = document.createElement('h1');
+
+  card.classList.add('platform-card');
+  cardTitle.textContent = stream.platform;
+  cardImage.src = stream.logoPath;
+
+  card.onclick = () => {
+    window.open(stream.href, '_blank');
+  };
+
+  card.appendChild(cardImage);
+  card.appendChild(cardTitle);
+
+  streamingPlatformsContainer.appendChild(card);
+}
+
+function scrollToTop() {
+  const home = document.getElementById('home');
+  home.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToStreaming() {
+  const streamingContainer = document.getElementById('streaming');
+  streamingContainer.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToDiscography() {
+  const discographyContainer = document.getElementById('discography');
+  discographyContainer.scrollIntoView({ behavior: 'smooth' });
+}
+
+const cardContainer = document.querySelector('.slides');
+let cards = [];
+let i = 0;
+discography.forEach((data) => {
+  const card = document.createElement('div');
+  const img = document.createElement('img');
+  const title = document.createElement('span');
+  const date = document.createElement('span');
+
+  title.classList.add('title');
+  card.classList.add('slide-card');
+  date.classList.add('date');
+  img.src = data.imgUrl;
+  img.alt = data.title;
+
+  title.textContent = data.title;
+  date.textContent = data.date;
+
+  card.appendChild(title);
+  card.appendChild(img);
+  card.appendChild(date);
+
+  cards.push(card);
+
+  cardContainer.appendChild(card);
+});
+
+let currentSlide = 0;
+function slideImages(direction) {
+  const slidesContainer = document.querySelector('.slides');
+  currentSlide += direction;
+
+  if (currentSlide >= discography.length) {
+    currentSlide = 0;
+  } else if (currentSlide < 0) {
+    currentSlide = discography.length - 1;
+  }
+
+  slidesContainer.style.transform = `translateX(-${currentSlide * 25}rem)`;
+}
+
+function openHamburgerMenu() {
+  const menuList = document.querySelector('#navbar .menu ol');
+  if (menuList.style.display === 'none') {
+    menuList.style.display = 'flex';
+  } else {
+    menuList.style.display = 'none';
+  }
+}
